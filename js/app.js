bdedc7121a73c25d4e9c7158c89c506691769921
Kristine Yates <kristine.e.yates@gmail.com>
2018-07-25 10:51:18 -0500

    Hero is on the board, moving around

--- js/app.js
+++ js/app.js
@@ -1,30 +1,68 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor(){
+    this.x = 0;
+    this.y = 0;
     this.sprite = 'images/enemy-bug.png';
-};
+  }
+  //draw enemy on board
+  render(){
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  //update enemy position - use dt - delta time between ticks
+  //reset position when they hit end of board
+  update(dt) {
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+  }
+}
+ const bugs = new Enemy();
+ const allEnemies = [];
+ allEnemies.push(bugs);
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Hero {
+  constructor(){
+    this.x = 101*2;
+    this.y = 83*5;
+    this.forward = 83;
+    this.side = 101;
+    this.sprite = 'images/char-boy.png';
+  }
+  //methods
+      //update position on board & check for crashes or wins
+  update(){
 
+  }
+      //render pic
+  render(){
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+      //handleInput allowedKeys
+      // @param {string} input
+  handleInput(input){
+    switch(input) {
+      case 'left':
+        this.x -= this.side;
+        break;
+      case 'up':
+        this.y -= this.forward;
+        break;
+      case 'right':
+        this.x += this.side;
+        break;
+      case 'down':
+        this.y += this.forward;
+        break;
+    }
+
+
+  }
+      //upon win, reset player position
+}
+
+const player = new Hero();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
